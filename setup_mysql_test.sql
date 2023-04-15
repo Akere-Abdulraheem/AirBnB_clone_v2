98dde3725203b0200ea620015c8f76010ea22dd7
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-04-15 21:20:19 +0000

    MySQL setup development and test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+-- a script that prepares a MySQL server for the project:
+-- A database hbnb_test_db
+-- a new user hbnb_test (in localhost)
+-- The password of hbnb_test should be set to hbnb_test_pwd
+-- hbnb_test should have all privileges on the database hbnb_test_db (and only this database)
+-- hbnb_test should have SELECT privilege on the database performance_schema (and only this database)
+-- If the database hbnb_test_db or the user hbnb_test already exists, your script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbnd_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- WITH GRANT OPTION
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- WITH GRANT OPTION;
